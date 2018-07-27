9aecb2689291ef81a78237195aa39ddce305c473
Ayk <aik_davtyan@mail.ru>
2018-07-27 17:01:24 +0300

    Создана модель фото

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_25_133734) do
+ActiveRecord::Schema.define(version: 2018_07_27_131313) do
+
+  create_table "photos", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
